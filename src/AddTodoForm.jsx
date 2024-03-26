30963540f4ed8a0e01f585307eebb2c5ff6b8662
SonaliBedge <bedgesonali@yahoo.com>
2024-03-26 12:12:21 -0700

    Lifting State and Props Handling

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,30 +1,46 @@
-function AddTodoForm(props) {
-    let todoTitle = "";
+import * as React from "react";
 
-    const handleAddTodo = (event) => {
-        event.preventDefault();
-        const todoTitle = event.target.title.value; // Access input value directly
-        // const todoTitle = event.currentTarget.elements.title.value;
-        props.onAddTodo(todoTitle);  //I have error in this line
-        console.log(todoTitle);
-      };
-      
-    return (
-      <div>
+function AddTodoForm({ addTodo }) {
+  const [todoTitle, setTodoTitle] = React.useState(""); //Creating new state variable named todoTitle with setter setTodoTitle
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    console.log(todoTitle);
+    const newTodo = {
+      title: todoTitle,
+      id: Date.now(), // Generate a unique identifier using Date.now()
+    };
+    addTodo(newTodo);
+    console.log(newTodo);
+    setTodoTitle(""); // Resetting todoTitle state to an empty string
+  };
+
+  return (
+    <div>
+      <hr />
+      <h1>Form</h1>
+      <hr />
+      <form onSubmit={handleAddTodo}>
+        <span>
+          <label htmlFor="todoTitle">
+            Title :{" "}
+            <input
+              type="text"
+              name="title"
+              id="todoTitle"
+              value={todoTitle}
+              onChange={handleTitleChange}
+            />
+          </label>
+        </span>
         <hr />
-        <h1>Form</h1>
-        <hr />
-        <form onSubmit={handleAddTodo}>
-          <span>
-            <label htmlFor="todoTitle">
-              Title : <input type="text" name="title" id="todoTitle" />
-            </label>
-          </span>
-          <hr />
-          <input type="submit" value="Add" />
-        </form>
-      </div>
-    );
-  }
-  export default AddTodoForm;
-  
+        <input type="submit" value="Add" />
+      </form>
+    </div>
+  );
+}
+export default AddTodoForm;
--- src/TodoListItem.jsx
+++ src/TodoListItem.jsx
@@ -1,15 +1,15 @@
-
-function TodoListItem(props){
-    const { todo } = props;
-    return (
-        <li>
-            <span>
-              <a href={todo.url}>{todo.title}</a>
-            </span>
-            <span> {todo.author}</span>
-            <span> {todo.num_comments}</span>
-            <span> {todo.points}</span>
-        </li>
-);
+function TodoListItem({ todo}) {
+  const { title, author, num_comments, points, url } = todo
+  // const { todo } = props;
+  return (
+    <li>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span> {author}</span>
+      <span> {num_comments}</span>
+      <span> {points}</span>
+    </li>
+  );
 }
 export default TodoListItem;
