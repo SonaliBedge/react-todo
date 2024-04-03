20f006fe73da2801461f905c013a171347888c7e
SonaliBedge <bedgesonali@yahoo.com>
2024-04-02 22:14:23 -0700

    Hooks and Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,25 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import "./App.css";
 
+const useSemiPersistentState = () => {
+  const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList')) || [];
+  // const savedTodoList = JSON.stringify(localStorage.getItem('savedTodoList')) || [];
+  const [todoList, setTodoList] = React.useState(savedTodoList); // Create new state variable named todoList with setter setTodoList and default value of an empty Array
+//Define a useEffect react hool with todoList as a dependency
+React.useEffect(() => {
+  localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+},[todoList]
+);
+// return [todoList, setTodoList];
+return [todoList, setTodoList];
+};
 function App() {
-  const [todoList, setTodoList] = React.useState([]); // Create new state variable named todoList with setter setTodoList and default value of an empty Array
-  const addTodo = (newTodo) => {
+   const [todoList, setTodoList] = useSemiPersistentState(); // Create new state variable named todoList with setter setTodoList and default value of an empty Array
+   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   };
   return (
+    <React.Fragment>
     <div>
       <hr />
       <TodoList todoList={todoList} />{" "}
@@ -17,7 +30,7 @@ function App() {
       <hr />
       <AddTodoForm addTodo={addTodo} />
     </div>
-
+    </React.Fragment>
   );
 }
 
