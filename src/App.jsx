1c259657356c951542ade1a83f27428c49de4b16
SonaliBedge <bedgesonali@yahoo.com>
2024-04-18 18:11:34 -0700

    Asyn Data, Conditional Rendering..

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,54 @@ import TodoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 import "./App.css";
 
-const useSemiPersistentState = () => {
-  const savedTodoList = JSON.parse(localStorage.getItem("savedTodoList")) || [];
-  const [todoList, setTodoList] = React.useState(savedTodoList);
-  React.useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
-  return [todoList, setTodoList];
-};
-
 function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
+  React.useEffect(() => {
+    setIsLoading(true);
+    new Promise((resolve) => {
+      setTimeout(
+        () =>
+          resolve({
+            data: {
+              todoList: JSON.parse(localStorage.getItem("savedTodoList")) || [],
+            },
+          }),
+        2000
+      );
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    if (isLoading === false) {
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+      const savedTodoList1 = JSON.parse(localStorage.getItem("savedTodoList"));
+    }
+  }, [todoList, isLoading]);
+
   const removeTodo = (id) => {
     const newTodoList = todoList.filter((todo) => id !== todo.id);
     setTodoList(newTodoList);
   };
-  const addTodo = (newTodo) => {
-    setTodoList([...todoList, newTodo]);
-  };
+
   return (
     <>
       <hr />
-      <TodoList todoList={todoList} onRemoveTodo={removeTodo} /> <hr />
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <TodoList todoList={todoList} onRemoveTodo={removeTodo} /> <hr />
+        </>
+      )}
       <AddTodoForm addTodo={addTodo} />
     </>
   );
