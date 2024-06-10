234c5901a2ca73e5a825fe33da07438650b9c5cc
SonaliBedge <bedgesonali@yahoo.com>
2024-06-09 22:46:11 -0700

    Sorting

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [sortOrder]);
+
+  // useEffect(() => {
+  //   console.log("sort", sortOrder);
+  // }, [sortOrder]);
 
   const fetchData = async () => {
     const options = {
@@ -19,9 +24,22 @@ function App() {
         Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
       },
     };
+
+    let sortField;
+    let i;
+    if (sortOrder === "CompletedAt") {
+      sortField = "CompletedAt";
+      i = 1;
+    } else {
+      i = 0;
+      sortField = "Title";
+    }
+
     const url = `https://api.airtable.com/v0/${
       import.meta.env.VITE_AIRTABLE_BASE_ID
-    }/${import.meta.env.VITE_TABLE_NAME}`;
+    }/${
+      import.meta.env.VITE_TABLE_NAME
+    }?view=Grid%20view&sort[0][field]=${sortField}&sort[0][direction]=${sortOrder}`;
 
     try {
       const response = await fetch(url, options);
@@ -30,14 +48,27 @@ function App() {
         throw new Error(message);
       }
       const data = await response.json();
-
-      const todos = data.records.map((todo) => {
-        const newTodo = {
-          id: todo.id,
-          title: todo.fields.Title,
-        };
-        return newTodo;
+      console.log(data.records);
+      data.records.sort((objectA, objectB) => {
+        const titleA = objectA.fields.Title.toLowerCase();
+        const titleB = objectB.fields.Title.toLowerCase();
+        console.log(objectB.fields);
+        if (sortOrder === "asc") {
+          if (titleA < titleB) return -1;
+          if (titleA > titleB) return 1;
+          return 0;
+        } else {
+          if (titleA < titleB) return 1;
+          if (titleA > titleB) return -1;
+          return 0;
+        }
       });
+      const todos = data.records.map((todo) => ({
+        id: todo.id,
+        title: todo.fields.Title,
+        CompletedAt: todo.fields.CompletedAt,
+      }));
+
       setTodoList(todos);
       setIsLoading(false);
     } catch (error) {
@@ -69,7 +100,6 @@ function App() {
         const message = `Error has occurred: ${response.status}`;
         throw new Error(message);
       }
-
       const data = await response.json();
 
       const addTodo = {
@@ -82,7 +112,12 @@ function App() {
       console.error("Error adding todo:", error.message);
     }
   };
-
+  const toggleSort = (sortValue) => {
+    setSortOrder(sortValue);
+  };
+  const togglesort = () => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
   const removeTodo = (id) => {
     const newTodoList = todoList.filter((todo) => id !== todo.id);
     setTodoList(newTodoList);
@@ -99,7 +134,16 @@ function App() {
                 <p>Loading...</p>
               ) : (
                 <>
-                  <TodoList todoList={todoList} onRemoveTodo={removeTodo} />
+                  <TodoList
+                    todoList={todoList}
+                    onRemoveTodo={removeTodo}
+                    onClick={togglesort}
+                    onChangeSortOrder={toggleSort}
+                    sortOrder={sortOrder}
+                  />
+                  {/* <button type="button" onClick={togglesort}>
+                    Sort
+                  </button> */}
                 </>
               )}
               <AddTodoForm addTodo={addTodo} />
@@ -113,3 +157,11 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <TodoList
+todoList={todoList}
+onRemoveTodo={removeTodo}
+onClick={togglesort}
+/> */
+}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,30 @@
 import TodoListItem from "./TodoListItem";
 import style from "./TodoListItem.module.css";
 import PropTypes from "prop-types";
-function TodoList({ todoList, onRemoveTodo }) {
+function TodoList({
+  todoList,
+  onRemoveTodo,
+  onClick,
+  sortOrder,
+  onChangeSortOrder,
+}) {
+  // function TodoList({ todoList, onRemoveTodo }) {
+  const handleSortChange = (e) => {
+    onChangeSortOrder(e.target.value);
+  };
   return (
     <div className={style.Container}>
       <h1 className={style.TitleFont}>Todo List</h1>
       <hr />
+      <button type="button" className={style.toggleButton} onClick={onClick}>
+        Sort
+      </button>
+      <select value={sortOrder} onChange={handleSortChange} className={style.toggleSelect} >
+        <option value="asc">Sort By</option>
+        <option value="asc">Asc</option>
+        <option value="desc">Desc</option>
+        <option value="CompletedAt">Created Date</option>
+      </select>
       <ul>
         {todoList.map((todo) => (
           <TodoListItem key={todo.id} todo={todo} onRemoveTodo={onRemoveTodo} />
@@ -18,5 +37,9 @@ function TodoList({ todoList, onRemoveTodo }) {
 TodoList.propTypes = {
   todoList: PropTypes.object,
   onRemoveTodo: PropTypes.func,
+  onClick: PropTypes.func,
+  onChangeSortOrder: PropTypes.func,
+  sortOrder: PropTypes.string.isRequired,
+  // sortOrder: PropTypes.oneof(["asc", "desc", "createdDate"])
 };
 export default TodoList;
